a8094675724a1d8153858e6abbf2665febafb115
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-04 15:10:49 +0500

    loops,function,array,objects

--- Javascript/script.js
+++ Javascript/script.js
@@ -20,32 +20,79 @@ const pi=3.14;
 //types refrence and primitives
 //refrence=>(),{},[]
 //premitives eg
-var a=12;
-var b=a;
-b=b+1;
-console.log(a,b)
+// var a=12;
+// var b=a;
+// b=b+1;
+// console.log(a,b)
 
 //refrence eg
 var x=[1,2,3];
 var y=x;
 y.pop();
 y.push(4);
-console.log(x,y)
+// console.log(x,y)
 
 
 //conditional
 no=4;
 if(no>10){
-    console.log("no is greater than 10");
+    //console.log("no is greater than 10");
 }
 else {
         if(no<10){
-        console.log("no is less than 10");
+        //console.log("no is less than 10");
     }
 
         else{
-            console.log("enter valid no ")
+            //console.log("enter valid no ")
         }
 }
+
+//loop=> for , while
+
+for(var i=1;i<=20;i++){
+    //console.log(i);
+}
+var i=1
+while(i<=20){
+    //console.log(i);
+    i++;
+}
+
+//function
+ function add(a,b,c){//=>parameters
+    sum=a+b+c;
+    //console.log(sum);
+ }
+ //add(1,2,3);//=>arguments
+
+ //array
+ var marks=[23,76,98]
+ //console.log(marks[0])//access array
+ //marks.push(77);
+ //console.log(marks)
+ //marks.pop()
+ 
+ //marks.shift()
+ //marks.unshift(68)
+ marks.splice(1,2)
+
+ //console.log(marks)
+
+ //object
+
+ //blank obj
+ var student={};
+ //filled object
+ var employee={
+    name:"alen",
+    id:2345,
+    rank:"manager"
+ }
+ console.log(employee);
+ console.log(employee.id)
+ //change obj key value
+ employee.rank="Senior manager";
+ console.log(employee.rank)
 
 
