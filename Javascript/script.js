ef9a60e6ca4026521a28d849ae84562164069c7a
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-04 14:28:24 +0500

    js quick rev

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+//word and keyword
+// score=500;
+// if;
+
+//var constant let
+var temp=30;
+let good="man";
+const pi=3.14;
+
+//hoisting
+console.log(a,b); //i.e value exist => undefine
+var a=12;
